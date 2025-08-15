a077799f8739ae32651b947f4b9cc4fb2446be78
Arif Bukan Maulana <ariffmaulana01@gmail.com>
2025-08-15 21:04:04 +0700

    edit : seeder memroy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,37 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    // public function run(): void
+    // {
+    //     Author::factory(1000)->create();
+    //     Category::factory(3000)->create();
+    //     Book::factory(100000)->create();
+    //     Rating::factory(500000)->create();
+
+    //     User::factory()->create([
+    //         'name' => 'Test User',
+    //         'email' => 'test@example.com',
+    //     ]);
+    // }
+
     public function run(): void
     {
-        Author::factory(1000)->create();
-        Category::factory(3000)->create();
-        Book::factory(100000)->create();
-        Rating::factory(500000)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (app()->environment('production')) {
+            // Mode full untuk production requirement
+            // Ditandai dengan isi file env    APP_ENV=production
+            // Jika ditest pada laptop pribadi maka memory sql baka exhausted /limit (Jika Jumlah data ratusan ribu)
+            Author::factory(1000)->create();
+            Category::factory(3000)->create();
+            Book::factory(100000)->create();
+            Rating::factory(500000)->create();
+        } else {
+            // Mode kecil untuk local/tester 
+            // Ditandai dengan isi file env    APP_ENV=local. 
+            // Kalau seandainya nge tes di laptop pribadi
+            Author::factory(20)->create();
+            Category::factory(60)->create();
+            Book::factory(1000)->create();
+            Rating::factory(3000)->create();
+        }
     }
 }
